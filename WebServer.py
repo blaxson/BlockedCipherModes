5d977e855003347c668b1f30eb6ad5c1330e9825
Bryce Laxson <blaxson@calpoly.edu>
2021-02-12 12:06:51 -0800

    finished CBC byteflip attack.. Task 2

--- WebServer.py
+++ WebServer.py
@@ -36,12 +36,45 @@ def submit(cipher, iv):
 def verify(cipher_string, cipher, iv):
     pattern = ";admin=true;"
     
-    cipher_text = io.BytesIO(cipher_string)
+    cipher_text = io.BytesIO(cipher_string) #cipher_block
     plain_text_stream = io.BytesIO()
     cbc_decrypt(cipher_text, plain_text_stream, cipher, iv)
-    plain_text = plain_text_stream.getvalue()
-    plain_text = plain_text.decode('utf-8')
-    return pattern in plain_text
+    plain_text = plain_text_stream.getvalue() # plain_text_block
+    
+    # start cbc byte flip attack
+    encoded = pattern.encode('utf-8')
+    desired_block = bytearray(encoded) # desired_block
+    cipher_block = bytearray(cipher_string)
+
+    cipher_block[0:16] = manipulate_cipher(cipher_block[0:16], plain_text[16:32], desired_block, 0)
+ 
+    # decrypt cipher again, this time with the manipulated ciphertext
+    new_cipher_text = io.BytesIO(bytes(cipher_block))
+    new_plain_text_stream = io.BytesIO()
+    cbc_decrypt(new_cipher_text, new_plain_text_stream, cipher, iv)
+
+
+    new_plain_text = new_plain_text_stream.getvalue()
+   
+    # have to convert any 'plaintext' byte > 127 back to an ascii val.
+    # this is the garbled decryption that we don't care about 
+    new_plain_array = bytearray(new_plain_text)
+    for i in range(0,16):
+        new_plain_array[i] = new_plain_array[i] % 127
+    # convert back into plaintext from bytes
+    new_plain_text = bytes(new_plain_array).decode('utf-8')
+
+    return pattern in new_plain_text
+
+""" takes in the cipher block to overwrite, what your desired message is, and
+an offset of where you want the desired message to be relative to the cipher
+block; length of desired message must be less than cipher_block - offset """
+def manipulate_cipher(cipher_block, plain_text_block, desired_block, offset):
+    for i in range (offset, len(desired_block)):
+        #print("cipherblock before: {0} plain_text before: {1} desired block before: {2}".format(cipher_block[i], plain_text_block[i], desired_block[i-offset]))
+        cipher_block[i] = cipher_block[i] ^ plain_text_block[i] ^ desired_block[i - offset]
+        #print("cipherblock after xor: {}".format(cipher_block[i]))
+    return cipher_block
 
 if __name__ == "__main__":
     main()
